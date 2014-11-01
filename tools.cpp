cef2df2063073fbf14e42e3a6cf4d97c0d7c39fd
lunolabs <lunolabs@users.noreply.github.com>
2014-11-01 13:44:25 +0300

    Create tools.cpp

---
+++ tools.cpp
@@ -0,0 +1,15 @@
+#include "tools.hpp"
+#include <cstdlib>
+#include <unistd.h>
+
+int kbhit (void)
+{
+    struct timeval tv;
+    fd_set rdfs;
+    tv.tv_sec = 0;
+    tv.tv_usec = 0;          
+    FD_ZERO(&rdfs);
+    FD_SET (STDIN_FILENO, &rdfs);
+    select(STDIN_FILENO+1, &rdfs, NULL, NULL, &tv);
+    return FD_ISSET(STDIN_FILENO, &rdfs);                    
+}
